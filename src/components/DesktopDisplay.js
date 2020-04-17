e50431b2df6b3b0ef46f89ff7b80dc495c7fe50f
Rory Lewis <lewisroryjames@gmail.com>
2020-04-17 14:04:11 +0200

    updated description and work experience

--- src/components/DesktopDisplay.js
+++ src/components/DesktopDisplay.js
@@ -1,135 +1,138 @@
-import React from "react";
-import CarouselComp from "./CarouselComp";
-import Radium from "radium";
+import React from 'react';
+import CarouselComp from './CarouselComp';
+import Radium from 'radium';
 
 // Integrate nedb backend with this so the code is more readable
 const DesktopDisplay = () => {
     const styles = {
         carouselContainer: {
-            width: "90%",
-            height: "40vh",
-            marginLeft: "5%",
-            borderTop: "1px solid white",
-            paddingTop: "5vh",
-            paddingBottom: "5vh",
-            marginBottom: "3vh"
+            width: '90%',
+            height: '40vh',
+            marginLeft: '5%',
+            borderTop: '1px solid white',
+            paddingTop: '5vh',
+            paddingBottom: '5vh',
+            marginBottom: '3vh',
         },
         carouselHeadline: {
-            color: "tomato",
-            marginBottom: "5vh",
-            marginLeft: "1vw",
-            fontSize: "2em"
+            color: 'tomato',
+            marginBottom: '5vh',
+            marginLeft: '1vw',
+            fontSize: '2em',
         },
         imageContainer: {
-            width: "30vw"
+            width: '30vw',
         },
         imageSize: {
-            maxHeight: "23vh",
-            width: "24vh",
-            marginLeft: "15vw"
+            maxHeight: '23vh',
+            width: '24vh',
+            marginLeft: '15vw',
         },
         desktopContainer: {
-            "@media (max-height: 720px) and (min-width: 800px)": {
-                marginTop: "30vh"
-            }
-        }
+            '@media (max-height: 720px) and (min-width: 800px)': {
+                marginTop: '30vh',
+            },
+        },
     };
     return (
         <div style={styles.desktopContainer}>
             <CarouselComp
                 styles={styles}
-                title="Recent Work"
+                title='Recent Work'
                 imagesArr={[
                     {
-                        imageUrl: "./images/cinuru.png",
+                        imageUrl: './images/hive_one.png',
                         imageText:
-              "Recently I have worked for Cinuru as a software developer. Their app is focused on connecting cinemas, users and user's favourite movies.",
-                        buttonDisplayText: "WEBSITE",
-                        buttonHref: "https://cinuru.com"
+                            'I have worked with Hive.one since August 2019 as a Frontend Developer, using React and all the latest best practices and libraries in web development.',
+                        buttonDisplayText: 'WEBSITE',
+                        buttonHref: 'https://hive.one',
                     },
                     {
-                        imageUrl: "./images/twist.jpg",
+                        imageUrl: './images/cinuru.png',
                         imageText:
-              "Open Sourced Twist bot for counting positive messages sent between channel users",
-                        buttonDisplayText: "GITHUB",
-                        buttonHref: "https://github.com/RL4444/twist_bot"
+                            "Recently I have worked for Cinuru as a software developer. Their app is focused on connecting cinemas, users and user's favourite movies.",
+                        buttonDisplayText: 'WEBSITE',
+                        buttonHref: 'https://cinuru.com',
                     },
                     {
-                        imageUrl: "./images/youtube.png",
-                        imageText:
-              "YouTube video seach engine powered by their YT api v3 and Material UI frontend",
-                        buttonDisplayText: "GITHUB",
-                        buttonHref: "https://github.com/RL4444/youtube-api-search-engine"
+                        imageUrl: './images/twist.jpg',
+                        imageText: 'Open Sourced Twist bot for counting positive messages sent between channel users',
+                        buttonDisplayText: 'GITHUB',
+                        buttonHref: 'https://github.com/RL4444/twist_bot',
                     },
                     {
-                        imageUrl: "./images/ulfs_cafe_homepage.PNG",
-                        imageText:
-              "In this project I worked as frontend developer, redesigning the cafe's website",
-                        buttonDisplayText: "WEBSITE",
-                        buttonHref: "https://ulfscafe.de"
+                        imageUrl: './images/youtube.png',
+                        imageText: 'YouTube video seach engine powered by their YT api v3 and Material UI frontend',
+                        buttonDisplayText: 'GITHUB',
+                        buttonHref: 'https://github.com/RL4444/youtube-api-search-engine',
                     },
                     {
-                        imageUrl: "./images/matchit.png",
+                        imageUrl: './images/ulfs_cafe_homepage.PNG',
+                        imageText: "In this project I worked as frontend developer, redesigning the cafe's website",
+                        buttonDisplayText: 'WEBSITE',
+                        buttonHref: 'https://ulfscafe.de',
+                    },
+                    {
+                        imageUrl: './images/matchit.png',
                         imageText:
-              "Full scale project that allows users to search if nearby Pubs are showing a football game they like",
-                        buttonDisplayText: "HEROKU",
-                        buttonHref: "https://match-it-football.herokuapp.com/#/"
-                    }
+                            'Full scale project that allows users to search if nearby Pubs are showing a football game they like',
+                        buttonDisplayText: 'HEROKU',
+                        buttonHref: 'https://match-it-football.herokuapp.com/#/',
+                    },
                 ]}
             />
             <CarouselComp
                 styles={styles}
-                title="Tech Stack"
+                title='Tech Stack'
                 imagesArr={[
                     {
-                        imageUrl: "./images/tech/react.png",
+                        imageUrl: './images/tech/react.png',
                         imageText:
-              "I have used the React library extensively for almost a year now and consider it the best tool for website development."
+                            'I have used the React library extensively for almost a year now and consider it the best tool for website development.',
                     },
                     {
-                        imageUrl: "./images/tech/graphql.png",
+                        imageUrl: './images/tech/graphql.png',
                         imageText:
-              "I have used Graph QL at a professional level and consider it a powerful tool for efficient backend"
+                            'I have used Graph QL at a professional level and consider it a powerful tool for efficient backend',
                     },
                     {
-                        imageUrl: "./images/tech/nodejs.png",
+                        imageUrl: './images/tech/nodejs.png',
                         imageText:
-              "Having worked at a professional level with Node.js on a daily basis, I am more than familiar with its functionality."
+                            'Having worked at a professional level with Node.js on a daily basis, I am more than familiar with its functionality.',
                     },
                     {
-                        imageUrl: "./images/tech/javascript.png",
-                        imageText: "Proficient in ES6/7 and Vanilla JS."
+                        imageUrl: './images/tech/javascript.png',
+                        imageText: 'Proficient in ES6/7 and Vanilla JS.',
                     },
                     {
-                        imageUrl: "./images/tech/sql.png",
+                        imageUrl: './images/tech/sql.png',
                         imageText:
-              "For complex data storage I find SQL to be the best solution. PostgreSQL is something I have used at a professional level and feel very comfortable in."
+                            'For complex data storage I find SQL to be the best solution. PostgreSQL is something I have used at a professional level and feel very comfortable in.',
                     },
                     {
-                        imageUrl: "./images/tech/css.png",
-                        imageText:
-              "I have used CSS for more than a year and I am very comfortable with it."
+                        imageUrl: './images/tech/css.png',
+                        imageText: 'I have used CSS for more than a year and I am very comfortable with it.',
                     },
                     {
-                        imageUrl: "./images/tech/serverless.png",
+                        imageUrl: './images/tech/serverless.png',
                         imageText:
-              "Serverless is a tool that I have some experience in from my time as Cinuru, and feel comfortable in setting up its configuration."
+                            'Serverless is a tool that I have some experience in from my time as Cinuru, and feel comfortable in setting up its configuration.',
                     },
                     {
-                        imageUrl: "./images/tech/jest.png",
+                        imageUrl: './images/tech/jest.png',
                         imageText:
-              "During my time at Cinuru GmbH I used wrote tests for backend API testing through Jest.js."
+                            'During my time at Cinuru GmbH I used wrote tests for backend API testing through Jest.js.',
                     },
                     {
-                        imageUrl: "./images/tech/aws.png",
+                        imageUrl: './images/tech/aws.png',
                         imageText:
-              "I have used Amazons Lambda services at a production level for monitoring backend check scripts."
+                            'I have used Amazons Lambda services at a production level for monitoring backend check scripts.',
                     },
                     {
-                        imageUrl: "./images/tech/google.png",
+                        imageUrl: './images/tech/google.png',
                         imageText:
-              "I am also comfortable with google's API service and have experience in using the YouTube V3 API engine."
-                    }
+                            "I am also comfortable with google's API service and have experience in using the YouTube V3 API engine.",
+                    },
                 ]}
             />
         </div>
--- src/components/WelcomeCenter.js
+++ src/components/WelcomeCenter.js
@@ -1,52 +1,52 @@
-import React from "react";
-import SocialButtons from "./SocialButtons";
-import CenterImage from "./CenterImage";
-import Radium, { StyleRoot } from "radium";
+import React from 'react';
+import SocialButtons from './SocialButtons';
+import CenterImage from './CenterImage';
+import Radium, { StyleRoot } from 'radium';
 
-const WelcomeCenter = props => {
+const WelcomeCenter = (props) => {
     const headlineStyle = {
-        margin: "0",
-        color: "white",
-        textAlign: "center",
-        fontSize: "6em",
-        "@media (max-width: 878px)": {
-            fontSize: "2.5em"
-        }
+        margin: '0',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: '6em',
+        '@media (max-width: 878px)': {
+            fontSize: '2.5em',
+        },
     };
     const headlineContainer = {
-        marginTop: "4vh",
-        "@media (min-width: 878px)": {
-            marginTop: "14vh"
-        }
+        marginTop: '4vh',
+        '@media (min-width: 878px)': {
+            marginTop: '14vh',
+        },
     };
     const welcomeCenterContainer = {
-        height: "80vh",
-        display: "flex",
-        flexDirection: "column",
-        width: "80%",
-        marginLeft: "10%",
-        justifyContent: "space-around center"
+        height: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        width: '80%',
+        marginLeft: '10%',
+        justifyContent: 'space-around center',
     };
     const subHeadStyle = {
-        color: "tomato",
-        fontSize: "4em",
-        "@media (max-width: 878px)": {
-            fontSize: "2em"
-        }
+        color: 'tomato',
+        fontSize: '4em',
+        '@media (max-width: 878px)': {
+            fontSize: '2em',
+        },
     };
     const imageContainer = {
-        marginTop: "2vh"
+        marginTop: '2vh',
     };
 
     const aboutMeSyle = {
-        height: "14vh",
-        color: "tomato",
-        transform: "translateY(0)",
-        transitionDuration: "600",
-        position: "inherit",
-        textAlign: "center",
-        fontSize: "1.5em",
-        marginTop: "3vh"
+        height: '14vh',
+        color: 'tomato',
+        transform: 'translateY(0)',
+        transitionDuration: '600',
+        position: 'inherit',
+        textAlign: 'center',
+        fontSize: '1.5em',
+        marginTop: '3vh',
     };
     let socialButtons = null;
     if (props.windowWidth < 1000) {
@@ -63,15 +63,12 @@ const WelcomeCenter = props => {
                     <h5>Full Stack Developer</h5>
                 </div>
                 <div onClick={props.toggleAboutMe} style={imageContainer}>
-                    <CenterImage
-                        windowHeight={props.windowHeight}
-                        windowWidth={props.windowWidth}
-                    />
+                    <CenterImage windowHeight={props.windowHeight} windowWidth={props.windowWidth} />
                 </div>
                 <div style={aboutMeSyle}>
                     <h5>
-            Juinor Full Stack Javascript Developer based in Berlin with
-            professional working experience in React, Graph QL, Node and others{" "}
+                        Frontend Javascript Developer based in Berlin with professional working experience in React,
+                        Redux, TypeScript, Docker, Styled Components, Graph QL, Node and others{' '}
                     </h5>
                 </div>
                 <div>{socialButtons}</div>
